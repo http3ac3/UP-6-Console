b1e3e37b57cd4e106f968b51bb0de6eb65b2bbb9
http3ac3 <ura19092004@gmail.com>
2022-11-30 20:22:03 +0300

    Добавьте файлы проекта.

---
+++ UP 6.1 Console Two-Dimensional/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_6._1_Console_Two_Dimensional
+{
+    internal class Program
+    {
+        static void OutArray(double[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int N, M;
+
+            Console.Write("Введите число строк массива: ");
+            while (!int.TryParse(Console.ReadLine(), out N) || N <= 0)
+            {
+                Console.Write("Были введены неверные данные! Число строк массива должно быть > 0! " +
+                    "Введите число строк массива: ");
+            }
+
+            Console.Write("Введите число столбцов массива: ");
+            while (!int.TryParse(Console.ReadLine(), out M) || M <= 0)
+            {
+                Console.Write("Были введены неверные данные! Число столбцов массива должно быть > 0! " +
+                    "Введите число столбцов массива: ");
+            }
+
+            double[,] array = new double[N, M];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write($"Введите значение элемента array[{i}, {j}]: ");
+                    while (!double.TryParse(Console.ReadLine(), out array[i, j]))
+                    {
+                        Console.Write("Были введены неверные данные! " +
+                            $"Введите значение элемента array[{i}, {j}]: ");
+                    }
+                }
+            }
+
+            Console.WriteLine("Изначальный двумерный массив: ");
+            OutArray(array);
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    if (array[i, j] < 0 && array[i, j] % 3 != 0) array[i, j] *= -1;
+
+            Console.WriteLine("Измененный двумерный массив: ");
+            OutArray(array);
+
+        }
+    }
+}
---
+++ UP 6.2 Console/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_6._2_Console
+{
+    internal class Program
+    {
+        static void OutArray(double[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        static void Main(string[] args)
+        {
+            int N;
+            Console.Write("Введите число элементов массива: ");
+            while (!int.TryParse(Console.ReadLine(), out N) || N <= 0)
+            {
+                Console.Write("Были введены неверные данные! Число элементов массива должно быть > 0! " +
+                    "Введите число элементов массива: ");
+            }
+            double[] array = new double[N];
+
+            Console.WriteLine("Заполните массив: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Введите значение элемента array[{i}]: ");
+                while (!double.TryParse(Console.ReadLine(), out array[i]))
+                {
+                    Console.Write("Были введены неверные данные! " +
+                        $"Введите значение элемента array[{i}]: ");
+                }
+            }
+
+            Console.Write("Изначальный массив: ");
+            OutArray(array);
+            Console.WriteLine();
+
+            double min = array.Min();
+
+            for (int i = 0; i < N; i++)
+            {
+                array[i] *= (array[i] == min) ? -1 : 1; 
+            }
+
+            Console.Write("Измененный массив: ");
+            OutArray(array);
+            Console.WriteLine();
+        }
+    }
+}
